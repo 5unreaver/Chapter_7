9cf267cb4c41ebe8d052850be5ef891fcef20cbb
5unreaver <avkme@bk.ru>
2025-08-08 16:09:35 +0300

    DOM1 chapter_7 page 259 commit

---
+++ src/A.java
@@ -0,0 +1,27 @@
+// Использование ключевого слова super для предотвращения
+// сокрытия имён
+class A {
+    int i;
+}
+
+class B extends A {
+    int i; // эта переменная скрывает переменную i из класса A
+
+    B(int a, int b) {
+        super.i = a; // переменная i из класса A
+        i = b; // переменная i из класса B
+    }
+
+    void show() {
+        System.out.println("i в суперклассе: " + super.i);
+        System.out.println("i в подклассе: " + i);
+    }
+}
+
+class UseSuper {
+    public static void main(String args[]) {
+        B subOb = new B(1, 2);
+
+        subOb.show();
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,106 @@
+class TruckDemo {
+    public static void main(String[] args) {
+//        Vehicle minivan = new Vehicle(7, 16, 21);
+//        Vehicle musclecar = new Vehicle(2, 14, 12);
+//
+//        int range, range2;
+//
+//        // Рассчитать дальность поездки при полном баке.
+//        range = minivan.mpg * minivan.fuelcap;
+//        System.out.println("Фургон может провезти " + minivan.passengers
+//                + " человек на " + range + " миль.");
+//
+//        range2 = musclecar.mpg * musclecar.fuelcap;
+//        System.out.println("Маслкар может провезти " + musclecar.passengers
+//                + " человек на " + range2 + " миль.");
+
+        // Создать ряд новых объектов типа Truck
+        Truck semi = new Truck(2, 200, 7, 44000);
+        Truck pickup = new Truck(3, 28, 15, 2000);
+        double gallons;
+        int dist = 252;
+
+        gallons = semi.fuelneeded(dist);
+
+        System.out.println("Грузовик может перевезти " + semi.getCargo()
+                + " фунтов.");
+        System.out.println("Для преодоления " + dist
+                + " миль грузовику требуется "
+                + gallons + " галлонов топлива.\n");
+
+        gallons = pickup.fuelneeded(dist);
+
+        System.out.println("Пикап может перевезти " + pickup.getCargo()
+                + " фунтов.");
+        System.out.println("Для преодоления " + dist
+                + " миль пикапу требуется "
+                + gallons + " галлонов топлива.\n");
+    }
+}
+
+class Vehicle {
+    private int passengers, fuelcap, mpg;
+
+    // Конструктор класса Vehicle
+    Vehicle(int p, int f, int m) {
+        passengers = p;
+        fuelcap = f;
+        mpg = m;
+    }
+
+    int range() {
+        return mpg * fuelcap;
+    }
+
+    // Определить объём топлива, необходимого
+    // ТС для преодоления заданного расстояния.
+    double fuelneeded(int miles) {
+        return (double) miles / mpg;
+    }
+
+    // Методы доступа к переменной экземпляра
+    int getPassengers() {
+        return passengers;
+    }
+
+    void setPassengers(int p) {
+        passengers = p;
+    }
+
+    int getFuelcap() {
+        return fuelcap;
+    }
+
+    void setFuelcap(int f) {
+        fuelcap = f;
+    }
+
+    int getMpg() {
+        return mpg;
+    }
+
+    void setMpg(int m) {
+        mpg = m;
+    }
+}
+
+class Truck extends Vehicle {
+    private int cargocap; // грузоподъёмность в фунтах
+
+    // Конструктор класса Truck
+    Truck(int p, int f, int m, int c) {
+        /* Инициализация членов класса Vehicle
+        с использованием конструктора этого класса. */
+        super(p, f, m);
+        cargocap = c;
+    }
+
+    // Методы доступа к переменно cargocap
+    int getCargo() {
+        return cargocap;
+    }
+
+    void putCargo(int c) {
+        cargocap = c;
+    }
+}
